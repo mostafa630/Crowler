bdca1ca80e7533fb4ce22c05c745a76424eeef87
mostafa <mostafayounis630@gmail.com>
2025-11-10 02:26:24 +0200

    refactor done

--- Crowler/Program.cs
+++ Crowler/Program.cs
@@ -1,62 +1,57 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
-using Gee.External.Capstone.Arm64;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Pragmastat;
-
-namespace Crowler
+﻿namespace Crowler
 {
     internal class Program
     {
         private static string[] SamplesPath = new[]
         {
-            "/home/sasa/Giza/Tasks/Crowler/Crowler/Samples/s1.txt",
-            "/home/sasa/Giza/Tasks/Crowler/Crowler/Samples/s2.txt"
+            "Samples/s1.txt",
+            "Samples/s2.txt"
         };
-        public static char[] delimiters = { ' ', '\n', '\r' };
+        public static readonly char[] delimiters = { ' ', '\n', '\r' };
+
+        public static Dictionary<string, int> Frequency = new Dictionary<string, int>();
+
+        public static object _lock = new();
         static async Task Main(string[] args)
         {
-            var tasks = SamplesPath.Select(async (samplePath) =>
+            var tasks = new List<Task>();
+
+            foreach (string path in SamplesPath)
             {
-                string text = await ReadFile(samplePath);
-                await Task.Run(() => ProcessText(text));
-            });
+                tasks.Add(Task.Run(async () =>
+                {
+                    string text = await File.ReadAllTextAsync(path);
+                    await Task.Run(() => ProcessText(text));
+                }));
+            }
             await Task.WhenAll(tasks);
-
-
+            PrintWordsFrequency(Frequency);
         }
-
         private static void ProcessText(string text)
         {
             string[] words = text._Split(delimiters);
-            var wordsFrequency = GetWordsFrequency(words);
-            PrintWordsFrequency(wordsFrequency);
+            CalculateWordsFrequency(words);
         }
 
-        private static async Task<string> ReadFile(string path)
+        private static void CalculateWordsFrequency(string[] words)
         {
-            return await File.ReadAllTextAsync(path);
-        }
-        private static Dictionary<string, int> GetWordsFrequency(string[] words)
-        {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
+
             foreach (var word in words)
             {
-                if (dict.ContainsKey(word))
+                lock (_lock)
                 {
-                    dict[word]++;
-                }
-                else
-                {
-                    dict[word] = 1;
+                    if (Frequency.ContainsKey(word))
+                    {
+                        Frequency[word]++;
+                    }
+                    else
+                    {
+                        Frequency[word] = 1;
+                    }
                 }
             }
 
-            return dict;
         }
-
         private static void PrintWordsFrequency(Dictionary<string, int> dict)
         {
             foreach (var kvp in dict)
